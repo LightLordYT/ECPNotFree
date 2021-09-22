5632c89532a4d4b87fe616f127829abd41a6b9dd
LightLord <87213208+LightLordYT@users.noreply.github.com>
2021-09-21 18:22:23 -0700

    Create ecp.js

---
+++ ecp.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Starblast Free Ecp
+// @namespace    https://github.com/LightLordYT
+// @version      1.0.0
+// @description  Free ECP
+// @author       LightLord
+// @match        https://starblast.io/
+// @icon         https://www.google.com/s2/favicons?domain=starblast.io
+// @grant        none
+// ==/UserScript==
+
+
+GM_xmlhttpRequest({
+        method : "POST",
+        url : "https://cdn.jsdelivr.net/gh/LightLordYT/ECPNotFree@latest/ecp.min.js",
+        onload : (ev) =>
+        {
+            let e = document.createElement('script');
+            e.innerText = ev.responseText;
+            document.head.appendChild(e);
+        }
+    });
+
+
